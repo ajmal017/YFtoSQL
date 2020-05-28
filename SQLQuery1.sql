75baede08439bd556c8ead88d9ec3a7477eac3e3
Juan Manuel Avila <juan_avila585@hotmail.com>
2020-05-28 13:49:22 -0300

    Version 1.0.0.2 where has been added the column TicketId to the tickets table, so then we can related witk TicketInfo table.

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿SELECT COUNT(*)
+  FROM FinancialDB.dbo.NewsInfo
---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿IF OBJECT_ID('NewsInfo') IS NOT NULL
+BEGIN
+    SELECT * FROM FinancialDB.dbo.NewsInfo
+END
+ELSE
+BEGIN
+    CREATE TABLE FinancialDB.dbo.NewsInfo (
+    Id int IDENTITY(1,1) PRIMARY KEY,
+    Fecha  smalldatetime NOT NULL,
+    Seccion  varchar(100),
+    Encabezado varchar(500),
+    Web  varchar(500)
+    )
+END
